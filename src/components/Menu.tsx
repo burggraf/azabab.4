18185431fca3225dc6b0614276c504085c953a9b
Mark Burggraf <markb@mantisbible.com>
2022-04-02 07:17:59 -0700

    removed unused defs

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -21,8 +21,6 @@ const appPages: AppPage[] = [
 	},
 ]
 
-const labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders']
-
 const Menu: React.FC = () => {
 	const location = useLocation()
 
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,13 +1,7 @@
 import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { useParams } from 'react-router';
-
-import ExploreContainer from '../components/ExploreContainer';
-
 import './Dashboard.css';
 
 const Dashboard: React.FC = () => {
-
-  const { name } = useParams<{ name: string; }>();
 
   return (
     <IonPage>
