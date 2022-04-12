de7508e0c5ba03c8a59e98a2774931e877cb42b6
Mark Burggraf <markb@mantisbible.com>
2022-04-12 05:53:33 -0700

    updated groups delete function to be a security definer, changed RLS delete policy to false

--- sql/functions/groups_delete.sql
+++ sql/functions/groups_delete.sql
@@ -1,18 +1,17 @@
 --DROP FUNCTION groups_delete;
-
 CREATE OR REPLACE FUNCTION groups_delete (target uuid)
 RETURNS TEXT as
 $$
 BEGIN
-
    IF (SELECT count(*) from groups where parent_id = target) > 0 THEN
       RAISE EXCEPTION 'children exist, cannot delete'; 
+   END IF;
+   IF (SELECT count(*) from groups_access where group_id = target AND user_id = auth.uid() AND access = 'admin') = 0 THEN
+      RAISE EXCEPTION 'admin access required to delete'; 
    END IF;
    DELETE FROM groups_access WHERE group_id = target;
    DELETE FROM groups WHERE id = target;
    RETURN 'OK';
 END
 $$
-language PLPGSQL;
-
-
+LANGUAGE PLPGSQL SECURITY DEFINER;
