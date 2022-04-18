bfffdce0cb1ca6a1aab6845005faf68f0b056443
Mark Burggraf <markb@mantisbible.com>
2022-04-18 08:03:17 -0500

    grid size chooser

--- src/pages/games/Griddy/Griddy.tsx
+++ src/pages/games/Griddy/Griddy.tsx
@@ -1,19 +1,21 @@
 import { IonBackButton, IonButtons, IonContent, IonHeader, 
-    IonPage, IonTitle, IonToolbar, IonGrid, IonRow, IonCol } from '@ionic/react';
+    IonPage, IonTitle, IonToolbar, IonGrid, IonRow, IonCol, IonMenuButton, IonButton, IonIcon, IonSelect, IonSelectOption, IonItem } from '@ionic/react';
 import { useCallback, useEffect, useState } from 'react'
 import GriddyService from './griddy.service';
 
 //import { useParams } from 'react-router';
 //import ExploreContainer from '../components/ExploreContainer';
 import './Griddy.css';
+import { refreshCircle, refreshCircleOutline } from 'ionicons/icons';
 const griddyService = GriddyService.getInstance();
 
 const Griddy: React.FC = () => {
-  const GRID_SIZE = 3;
+  //const GRID_SIZE = 4;
+  const [GRID_SIZE, setGRID_SIZE] = useState(4);
   const [queue,setQueue] = useState<string[]>([])
   const [choices,setChoices] = useState<string[]>([])
   const [activeChoice,setActiveChoice] = useState<number>(-1)
-  const [board,setBoard] = useState<string[][]>([...Array(GRID_SIZE)].map(x=>Array(GRID_SIZE).fill('')) )
+  const [board,setBoard] = useState<string[][]>([...Array(GRID_SIZE)].map(x=>Array(GRID_SIZE).fill('')))
   const [score,setScore] = useState<number>(0)
   const [successfulWords,setSuccessfulWords] = useState<string>('')
   // const { name } = useParams<{ name: string; }>();
@@ -57,14 +59,11 @@ const Griddy: React.FC = () => {
   useEffect(() => {
     setChoices(queue.slice(0,GRID_SIZE));
     if (queue.length === 0 && board[GRID_SIZE-1][GRID_SIZE-1] !== '') {
-      console.log('**** DONE ****');
       calculateScore();
     }
   },[queue, calculateScore, board])
 
   const toggleChoiceBox = (i:number) => {
-    console.log('toggleChoiceBox', i)
-    console.log('activeChoice', activeChoice)
     const item = document.getElementById(`choice${i}`);
     if (activeChoice === i) {
       item?.classList.toggle('selected');
@@ -88,7 +87,6 @@ const Griddy: React.FC = () => {
       console.log('placeChoice: no active choice');
       return;
     }
-    console.log('placeChoice', row, col)
     const choice = choices[activeChoice];
     const newBoard = [...board];
     newBoard[row][col] = choice;
@@ -106,23 +104,49 @@ const Griddy: React.FC = () => {
       }
     },100);
   }
+  const reset = async (newSize: number = GRID_SIZE) => {
+    console.log('reset');
+    setBoard([...Array(newSize)].map(x=>Array(newSize).fill('')));
+    init();
+
+  }
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonButtons slot="start">
-            <IonBackButton defaultHref='/login' />
+            <IonMenuButton />
           </IonButtons>
           <IonTitle>Griddy</IonTitle>
+          <IonButtons slot="end">
+            <IonSelect value={GRID_SIZE} 
+                      className="grid_size_selector"
+                      okText="OK" 
+                      cancelText="Cancel" 
+                      interface="popover"
+                      interfaceOptions={ {header: 'Grid Size'} }
+                      onIonChange={(e: any) => {setGRID_SIZE(parseInt(e.detail.value,10));reset(parseInt(e.detail.value,10));}}
+                      placeholder={GRID_SIZE.toString()}>
+              <IonSelectOption value="3">3 x 3</IonSelectOption>
+              <IonSelectOption value="4">4 x 4</IonSelectOption>
+              <IonSelectOption value="5">5 x 5</IonSelectOption>
+              <IonSelectOption value="6">6 x 6</IonSelectOption>
+            </IonSelect>
+            <IonButton color='primary' onClick={() => {reset();}}>
+							<IonIcon size='large' icon={refreshCircleOutline}></IonIcon>
+						</IonButton>
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
 
       <IonContent className="ion-padding">
       <div className="centeredDiv">
-        <IonGrid style={{width: '300px', maxWidth: '300px', minHeight: '100px', backgroundColor: 'lightgray'}}>
+        <IonGrid style={{width: '600px', maxWidth: '600px', minHeight: '100px'}}>
             <IonRow key='choices'>
               {choices.map((choice,choiceIndex) => (
                   <IonCol 
+                      style={{backgroundColor: 'lightgray'}}
                       key={`choice${choiceIndex}`} 
                       id={`choice${choiceIndex}`}
                       onClick={() => toggleChoiceBox(choiceIndex)}
@@ -134,7 +158,7 @@ const Griddy: React.FC = () => {
         </IonGrid>
         </div>
         <div className="centeredDiv">
-        <IonGrid style={{width: '300px', maxWidth: '300px'}}>
+        <IonGrid style={{width: '600px', maxWidth: '600px'}}>
             {board.map((row, rowIndex) => (
               <IonRow>
                 {row.map((col, colIndex) => (
